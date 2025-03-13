b51198daa9c8b7cf3aaa6233e5d8f5a4911926e5
Heavenly Cloudz <mails4anietor@gmail.com>
2025-03-12 19:59:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,7 +174,7 @@ st.sidebar.title("Controls")
 # Hyperparameter inputs
 epochs = st.sidebar.number_input("Number of epochs", min_value=1, max_value=100, value=10)
 batch_size = st.sidebar.number_input("Batch size", min_value=1, max_value=64, value=32)
-dataset_path = st.text_input("Please enter the path to your dataset:", value=r"C:\Users\Antoru Grace Inc\.vscode\CNN\streamlit_project\data\train")
+dataset_path = st.text_input("Please enter the path to your dataset:", value=r"C:\\Users\\Antoru Grace Inc\\.vscode\\CNN\\streamlit_project\\data")
 
 # Button to check the path
 if st.button("Check Path"):
