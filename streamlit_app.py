c722f18b9b6e11a8cc8006cda93bbd350c57114a
Heavenly Cloudz <mails4anietor@gmail.com>
2025-03-15 17:08:01 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,13 @@ import cv2
 import matplotlib.pyplot as plt
 from PIL import Image  # Importing PIL for image processing
 
+# Page configurations
+st.set_page_config(
+    page_title="Lung Cancer Detection",
+    page_icon="C:\Users\Antoru Grace Inc\Downloads\logo"
+    layout="wide" # Layout is wide
+)
+
 # Constants
 IMAGE_HEIGHT, IMAGE_WIDTH = 150, 150
 MODEL_FILE = os.path.join(os.path.dirname(__file__), 'lung_cancer_detection_model.h5')
